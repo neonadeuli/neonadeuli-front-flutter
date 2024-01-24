cf0ddae9a4af34ac6cc3e9539e979c05c5acb3cb
eora21 <eora21@naver.com>
2024-01-24 22:01:27 +0900

    feat: 🎸 추가 조회글 위젯 작성

---
+++ lib/widgets/post.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:neonadeuli_front_flutter/pages/post_detail_page.dart';
+
+class Post extends StatelessWidget {
+  final int postId;
+
+  const Post({super.key, required this.postId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Row(
+          children: [
+            Icon(
+              Icons.account_circle_rounded,
+              size: 40,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text('eora21')
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          width: double.infinity,
+          color: Colors.grey.withOpacity(0.1),
+          child: Image.network(
+            "https://media.discordapp.net/attachments/1193853621197086765/1199293754054545428/KakaoTalk_20240123_185826476_03.jpg?ex=65c20458&is=65af8f58&hm=420059aefd877e9b98ef885aaee012abd587eded6805e01d6c49cb33e133a02d&=&format=webp&width=514&height=686",
+            height: 300,
+            fit: BoxFit.contain,
+          ),
+        ),
+        Row(
+          children: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.thumb_up_alt_rounded)),
+            const Text('32'),
+            IconButton(
+                onPressed: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PostDetail(
+                            postId: postId,
+                          ),
+                        ),
+                      )
+                    },
+                icon: const Icon(Icons.comment_rounded)),
+            const Text('11'),
+          ],
+        ),
+        Hero(
+          tag: 'post$postId',
+          child: Material(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2,
+                  color: Colors.grey.withOpacity(0.8),
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Center(child: Text('메롱')),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
