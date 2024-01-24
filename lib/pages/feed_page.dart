11b5daeaf1a9efbbb46623f667493df25b839f13
eora21 <eora21@naver.com>
2024-01-24 22:03:42 +0900

    feat: 🎸 추가 피드 페이지에 조회글 위젯 띄우도록 작성
    추후 ListView.separated 적용할 것

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:neonadeuli_front_flutter/widgets/post.dart';
 
 class FeedPage extends StatelessWidget {
   const FeedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('피드 페이지');
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: ListView(
+        children: [
+          const Post(
+            postId: 1,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Container(
+              height: 3,
+              color: Colors.orange[200],
+            ),
+          ),
+          const Post(
+            postId: 2,
+          ),
+        ],
+      ),
+    );
   }
 }
