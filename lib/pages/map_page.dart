daa4c811d59a42a55e35e654bfb067ef2e621434
eora21 <eora21@naver.com>
2024-01-27 15:49:01 +0900

    feat: 🎸 맵 페이지에 구글맵 적용

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('지도 페이지');
+    return const GoogleMap(
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(30, 30),
+          zoom: 11.0,
+        ));
   }
 }
