42715865db93408f6ba1cf500beecf3504ba4cfb
lightbell03 <bellight28@gmail.com>
2024-01-29 21:03:06 +0900

    feat: 라우팅을 위해 go router 의존성 추가

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+      child: Text('data'),
+    );
+  }
+}
